b037b708790aa5f4f494301b2fd21cf357f76094
Yann <yann.crennsf@gmail.com>
2023-09-21 15:20:16 -0700

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CartContainer from "./CartContainer";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <CartContainer />
+    </div>
+  );
+}
+
+export default App;
